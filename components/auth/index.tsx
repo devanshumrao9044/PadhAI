ca401224d74038e7c77b307a34728c119db9e0a7
DEVANSH <224992621+devanshumrao9044@users.noreply.github.com>
2026-06-12 13:24:08 +0530

    Export authentication components from index.tsx

---
+++ components/auth/index.tsx
@@ -0,0 +1,6 @@
+export { default as LoginForm } from './LoginForm';
+export { default as SignupForm } from './SignupForm';
+export { default as AuthInput } from './AuthInput';
+export { default as AuthButton } from './AuthButton';
+export { default as AuthLogo } from './AuthLogo';
+
