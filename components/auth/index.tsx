0d2afcb18cb4ff21110da10652dc5500b2a07e0c
DEVANSH <224992621+devanshumrao9044@users.noreply.github.com>
2026-07-18 21:09:35 +0530

    Delete components/auth directory

--- components/auth/index.tsx
+++
@@ -1,6 +0,0 @@
-export { default as LoginForm } from './LoginForm';
-export { default as SignupForm } from './SignupForm';
-export { default as AuthInput } from './AuthInput';
-export { default as AuthButton } from './AuthButton';
-export { default as AuthLogo } from './AuthLogo';
-
