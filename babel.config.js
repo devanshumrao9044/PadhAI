510f374ee4b6e45b180e5b21c7a8516555880135
onspace.ai <onspace.ai@gmail.com>
2026-08-14 21:59:59 +0530

    Fix Hermes private class fields error

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(false)
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+    ],
   }
 }
